23be73b7261b3ef887be460a157840a923ed3955
James MacAdie <james.macadie@telerealtrillium.com>
2016-08-29 15:26:37 +0100

    Complete unit tests for UniqueFormula
    Filled in the shell unit tests previously created for this class

--- test/ExcelCompare2/UniqueFormulaTest.java
+++ test/ExcelCompare2/UniqueFormulaTest.java
@@ -18,15 +18,28 @@ import static org.junit.Assert.*;
  */
 public class UniqueFormulaTest {
     
+    private final CellRef cA1;
+    private final CellRef cA2;
+    private final Formula f;
+    private final UniqueFormula uf;
+    
     public UniqueFormulaTest() {
+        cA1 = new CellRef("A1");
+        cA2 = new CellRef("A2");
+        f = new Formula("=B1", cA1);
+        uf = new UniqueFormula(f);
     }
     
     @BeforeClass
     public static void setUpClass() {
+        System.out.println("   test UniqueFormula class");
+        System.out.println("=========================");
     }
     
     @AfterClass
     public static void tearDownClass() {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("");
     }
     
     @Before
@@ -42,14 +55,11 @@ public class UniqueFormulaTest {
      */
     @Test
     public void testCompareTo_String() {
-        System.out.println("compareTo");
-        String formulaR1C1 = "";
-        UniqueFormula instance = null;
-        int expResult = 0;
-        int result = instance.compareTo(formulaR1C1);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test CompareTo(String) method");
+        
+        assertEquals("'=RC[1]' is not the same formula as '=B1' in cell 'A1'", 0, uf.compareTo("=RC[1]"));
+        assertTrue("'=RC[2]' is not a smaller formula than '=B1' in cell 'A1'", (uf.compareTo("=RC[2]") < 0));
+        assertTrue("'=RC' is not a larger formula than '=B1' in cell 'A1'", (uf.compareTo("=RC") > 0));
     }
 
     /**
@@ -57,14 +67,16 @@ public class UniqueFormulaTest {
      */
     @Test
     public void testCompareTo_Formula() {
-        System.out.println("compareTo");
-        Formula formula = null;
-        UniqueFormula instance = null;
-        int expResult = 0;
-        int result = instance.compareTo(formula);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test CompareTo(Formula) method");
+        
+        Formula g = new Formula("=F3", new CellRef("E3")); // =RC[1]
+        assertEquals("'=F3' in cell 'E3' is not the same formula as '=B1' in cell 'A1'", 0, uf.compareTo(g));
+        
+        g = new Formula("=BB36", new CellRef("AZ36")); // =RC[2]
+        assertTrue("'=BB36' in cell 'AZ36' is not a smaller formula than '=B1' in cell 'A1'", (uf.compareTo(g) < 0));
+        
+        g = new Formula("=DEF3504", new CellRef("DEF3504")); // =RC
+        assertTrue("'=DEF3504' in cell 'DEF3504' is not a larger formula than '=B1' in cell 'A1'", (uf.compareTo(g) > 0));
     }
 
     /**
@@ -72,14 +84,18 @@ public class UniqueFormulaTest {
      */
     @Test
     public void testCompareTo_UniqueFormula() {
-        System.out.println("compareTo");
-        UniqueFormula formula = null;
-        UniqueFormula instance = null;
-        int expResult = 0;
-        int result = instance.compareTo(formula);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test CompareTo(UniqueFormula) method");
+        
+        UniqueFormula g = new UniqueFormula(new Formula("=F3", new CellRef("E3"))); // =RC[1]
+        assertEquals("'=F3' in cell 'E3' is not the same formula as '=B1' in cell 'A1'", 0, uf.compareTo(g));
+        g.addCell(cA1);
+        assertEquals("'=F3' in cell 'E3' is not the same formula as '=B1' in cell 'A1'", 0, uf.compareTo(g));
+        
+        g = new UniqueFormula(new Formula("=BB36", new CellRef("AZ36"))); // =RC[2]
+        assertTrue("'=BB36' in cell 'AZ36' is not a smaller formula than '=B1' in cell 'A1'", (uf.compareTo(g) < 0));
+        
+        g = new UniqueFormula(new Formula("=DEF3504", new CellRef("DEF3504"))); // =RC
+        assertTrue("'=DEF3504' in cell 'DEF3504' is not a larger formula than '=B1' in cell 'A1'", (uf.compareTo(g) > 0));
     }
 
     /**
@@ -87,12 +103,12 @@ public class UniqueFormulaTest {
      */
     @Test
     public void testAddCell() {
-        System.out.println("addCell");
-        CellRef newCell = null;
-        UniqueFormula instance = null;
-        instance.addCell(newCell);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test addCell() and getRange() methods");
+        
+        assertFalse("A2 should not be part of the unique formula yet", uf.getRange().contains(cA2));
+        
+        uf.addCell(cA2);
+        assertTrue("A2 should now be part of the unique formula", uf.getRange().contains(cA2));
     }
 
     /**
@@ -100,13 +116,8 @@ public class UniqueFormulaTest {
      */
     @Test
     public void testGetFormula() {
-        System.out.println("getFormula");
-        UniqueFormula instance = null;
-        String expResult = "";
-        String result = instance.getFormula();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test getFormula() method");
+        assertEquals("'=B1' is not the formula of the unique formula", "=B1", uf.getFormula());
     }
 
     /**
@@ -114,27 +125,9 @@ public class UniqueFormulaTest {
      */
     @Test
     public void testGetFormulaR1C1() {
-        System.out.println("getFormulaR1C1");
-        UniqueFormula instance = null;
-        String expResult = "";
-        String result = instance.getFormulaR1C1();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test getFormulaR1C1() method");
+        assertEquals("'=RC[1]' is not the formula of the unique formula", "=RC[1]", uf.getFormulaR1C1());
     }
 
-    /**
-     * Test of getRange method, of class UniqueFormula.
-     */
-    @Test
-    public void testGetRange() {
-        System.out.println("getRange");
-        UniqueFormula instance = null;
-        CompoundRange expResult = null;
-        CompoundRange result = instance.getRange();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
 }
