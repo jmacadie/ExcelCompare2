4d9c78191225f2baf99727e8f8eb08c5412f4e08
James MacAdie <james.macadie@telerealtrillium.com>
2016-08-30 05:34:27 +0100

    Complete unit tests for CondensedFormuale
    Filled in the shell unit tests previously created for this class

--- test/ExcelCompare2/CondensedFormulaeTest.java
+++ test/ExcelCompare2/CondensedFormulaeTest.java
@@ -5,7 +5,8 @@
  */
 package ExcelCompare2;
 
-import java.util.ListIterator;
+import java.util.List;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,15 +20,38 @@ import static org.junit.Assert.*;
  */
 public class CondensedFormulaeTest {
     
+    private final CellRef cA1;
+    private final CellRef cA2;
+    private final CellRef cB1;
+    private final CellRef cB2;
+    private final CellRef cAA32;
+    private final CondensedFormulae cf;
+    
     public CondensedFormulaeTest() {
+        cA1 = new CellRef("A1");
+        cA2 = new CellRef("A2");
+        cB1 = new CellRef("B1");
+        cB2 = new CellRef("B2");
+        cAA32 = new CellRef("AA32");
+        
+        List<Formula> f = new ArrayList<> ();
+        f.add(new Formula("=B1", cA1));
+        f.add(new Formula("=C1", cB1));
+        f.add(new Formula("=A3", cA2));
+        f.add(new Formula("=B3", cB2));
+        cf = new CondensedFormulae(f);
     }
     
     @BeforeClass
     public static void setUpClass() {
+        System.out.println("   test CondensedFormulae class");
+        System.out.println("=========================");
     }
     
     @AfterClass
     public static void tearDownClass() {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("");
     }
     
     @Before
@@ -43,69 +67,30 @@ public class CondensedFormulaeTest {
      */
     @Test
     public void testFindFormula() {
-        System.out.println("findFormula");
-        AnalysedFormula f = null;
-        CondensedFormulae instance = new CondensedFormulae();
-        CompoundRange expResult = null;
-        CompoundRange result = instance.findFormula(f);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findCell method, of class CondensedFormulae.
-     */
-    @Test
-    public void testFindCell() {
-        System.out.println("findCell");
-        CellRef find = null;
-        CondensedFormulae instance = new CondensedFormulae();
-        boolean expResult = false;
-        boolean result = instance.findCell(find);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setAnalysed method, of class CondensedFormulae.
-     */
-    @Test
-    public void testSetAnalysed_CellRef() {
-        System.out.println("setAnalysed");
-        CellRef analysed = null;
-        CondensedFormulae instance = new CondensedFormulae();
-        instance.setAnalysed(analysed);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setAnalysed method, of class CondensedFormulae.
-     */
-    @Test
-    public void testSetAnalysed_CompoundRange() {
-        System.out.println("setAnalysed");
-        CompoundRange analysed = null;
-        CondensedFormulae instance = new CondensedFormulae();
-        instance.setAnalysed(analysed);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of listIterator method, of class CondensedFormulae.
-     */
-    @Test
-    public void testListIterator() {
-        System.out.println("listIterator");
-        CondensedFormulae instance = new CondensedFormulae();
-        ListIterator<AnalysedFormula> expResult = null;
-        ListIterator<AnalysedFormula> result = instance.listIterator();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test findFormula() method");
+        
+        AnalysedFormula af;
+        CompoundRange expected;
+        
+        // Find matching instance (in range)
+        af = new AnalysedFormula(new Formula("=B1", cA1));
+        expected = new CompoundRange(cA1);
+        expected.addCell(cB1);
+        assertTrue("Matching refence (in range)", cf.findFormula(af).equals(expected));
+        
+        // Find matching instance (out of range)
+        af = new AnalysedFormula(new Formula("=AB32", cAA32));
+        assertTrue("Matching refence (out of range)", cf.findFormula(af).equals(expected));
+        
+        // Find the other matching instance
+        af = new AnalysedFormula(new Formula("=AA33", cAA32));
+        expected = new CompoundRange(cA2);
+        expected.addCell(cB2);
+        assertTrue("The other matching instance", cf.findFormula(af).equals(expected));
+        
+        // Find non-matching instance => returns null
+        af = new AnalysedFormula(new Formula("=AB37", cAA32));
+        assertEquals("Non-matching instance", null, cf.findFormula(af));
     }
 
     /**
@@ -113,14 +98,30 @@ public class CondensedFormulaeTest {
      */
     @Test
     public void testGetForumla() {
-        System.out.println("getForumla");
-        CellRef cell = null;
-        CondensedFormulae instance = new CondensedFormulae();
-        AnalysedFormula expResult = null;
-        AnalysedFormula result = instance.getForumla(cell);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test getForumla() method");
+        
+        CellRef cell;
+        AnalysedFormula expected;
+        
+        // Find matching instance
+        cell = new CellRef("A1");
+        expected = new AnalysedFormula(new Formula("=B1", cA1));
+        assertEquals("Matching refence", expected.getFormula(), cf.getForumla(cell).getFormula());
+        assertEquals("Matching refence R1C1", expected.getFormulaR1C1(), cf.getForumla(cell).getFormulaR1C1());
+        
+        // Find 2nd matching instance
+        cell = new CellRef("B1");
+        expected = new AnalysedFormula(new Formula("=C1", cB1));
+        assertEquals("2nd matching refence", expected.getFormulaR1C1(), cf.getForumla(cell).getFormulaR1C1());
+        
+        // Find the other matching instance
+        cell = new CellRef("B2");
+        expected = new AnalysedFormula(new Formula("=A3", cA2));
+        assertEquals("The other matching refence", expected.getFormula(), cf.getForumla(cell).getFormula());
+        assertEquals("The other matching refence R1C1", expected.getFormulaR1C1(), cf.getForumla(cell).getFormulaR1C1());
+        
+        // Find non-matching instance => returns null
+        assertEquals("Non-matching instance", null, cf.getForumla(cAA32));
     }
 
     /**
@@ -128,12 +129,9 @@ public class CondensedFormulaeTest {
      */
     @Test
     public void testDiff() {
-        System.out.println("diff");
-        CondensedFormulae from = null;
-        CondensedFormulae instance = new CondensedFormulae();
-        instance.diff(from);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("*   test diff() method");
+        
+        // Not sure how to test output to stdout?
     }
     
 }
