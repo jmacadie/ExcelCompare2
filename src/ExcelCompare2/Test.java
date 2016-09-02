8a7f9334d96d32a03d37388c532e76d338c54ec6
James MacAdie <james.macadie@telerealtrillium.com>
2016-09-02 18:09:45 +0100

    Update to main routine
    So we can test out the new mapping functions (which don't yet work!)

--- src/ExcelCompare2/Test.java
+++ src/ExcelCompare2/Test.java
@@ -5,6 +5,8 @@
  */
 package ExcelCompare2;
 
+import java.util.List;
+import java.util.ArrayList;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.Sheet;
 
@@ -18,7 +20,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        testCondense();
+        testRowMap();
     }
     
     private static void testCondense() {
@@ -42,6 +44,30 @@ public class Test {
             System.err.println("Failed: " + e.getMessage());
         }
         
+    }
+    
+    private static void testRowMap() {
+        
+        CellRef cA1 = new CellRef("A1");
+        CellRef cA2 = new CellRef("A2");
+        CellRef cB1 = new CellRef("B1");
+        CellRef cB2 = new CellRef("B2");
+        
+        List<Formula> f = new ArrayList<> ();
+        f.add(new Formula("=B1", cA1));
+        f.add(new Formula("=C1", cB1));
+        f.add(new Formula("=A3", cA2));
+        f.add(new Formula("=B3", cB2));
+        CondensedFormulae from = new CondensedFormulae(f);
+        
+        f = new ArrayList<> ();
+        f.add(new Formula("=B1", cA1));
+        f.add(new Formula("=C1", cB1));
+        f.add(new Formula("=A3", cA2));
+        f.add(new Formula("=B3", cB2));
+        CondensedFormulae to = new CondensedFormulae(f);
+        
+        CellTranslations.test(from, to);
     }
     
 }
