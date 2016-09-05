c837ba9a32a189260b2f03380a45275b592e9b92
James MacAdie <james.macadie@telerealtrillium.com>
2016-09-05 18:04:42 +0100

    SheetDiff should contain a list of CellDiffs
    Was only allowing for a single difference before but list will allow for
    multiple cell differences to be recorded

--- src/ExcelCompare2/SheetDiff.java
+++ src/ExcelCompare2/SheetDiff.java
@@ -5,6 +5,9 @@
  */
 package ExcelCompare2;
 
+import java.util.List;
+import java.util.LinkedList;
+
 /**
  *
  * @author james.macadie
@@ -12,11 +15,11 @@ package ExcelCompare2;
 public class SheetDiff {
     
     private final CellTranslations _translations;
-    private final CellDiff _differences;
+    private final List<CellDiff> _differences;
     
     SheetDiff () {
         _translations = new CellTranslations();
-        _differences = new CellDiff();
+        _differences = new LinkedList<> ();
     }
     
 }
