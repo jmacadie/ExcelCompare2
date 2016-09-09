02987862e4c7cf6e56edfd2800fff268a7f15cee
James MacAdie <james.macadie@telerealtrillium.com>
2016-09-09 09:22:27 +0100

    Remove redundant imports
    These were used when loadSpreadSheet resided in this class but have moved
    all that jazz into the POISpreadSheet class so can lose the imports

--- src/ExcelCompare2/SpreadSheetUtils.java
+++ src/ExcelCompare2/SpreadSheetUtils.java
@@ -5,13 +5,6 @@
  */
 package ExcelCompare2;
 
-import java.io.File;
-import java.io.IOException;
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-
 /**
  *
  * @author james.macadie
